506182a1e99b57c91743f779aa2a638c54f1936e
EnriqueAlapizco <enrique.alapizco@uabc.edu.mx>
2025-05-21 14:17:08 -0700

    Carga inicial
    Creacion del proyecto

---
+++ src/main/java/com/mycompany/tamagotchi/Tamagotchi.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tamagotchi;
+
+/**
+ *
+ * @author enriq
+ */
+public class Tamagotchi {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
